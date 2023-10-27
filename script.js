3edf8620083bcd405b535c294dd236f983bf8cf8
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2023-10-27 02:39:40 -0500

    Agregar funcionalidad a botones especiales

--- script.js
+++ script.js
@@ -2,18 +2,20 @@
 const botones = document.querySelectorAll("button");
 const entrada = document.getElementById("entrada");
 
-const entradaAnterior = {letra:"", categoria: ""};
-const entradaActual = {letra:"", categoria: ""};
+const entradaAnterior = { letra: "", categoria: "" };
+const entradaActual = { letra: "", categoria: "" };
 
-const numeros = ["0", "1", "2","3","4","5","6","7","8","9"];
+const numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const operadores = ["x", "/", "+", "-"];
 const punto = ["."];
 const abreParentesis = ["("];
 const cierraParentesis = [")"];
-const especiales = ["C", "B", "="];
+const especiales = ["C", "<", "="];
 
-const contadorParentesis = 0;
+var resultado = 0;
+var contadorParentesis = 0;
 var estado = "correcto";
+var continuar = true;
 
 const estructura = {
     numero: ["n", "o", ".", ")"],
@@ -21,51 +23,79 @@ const estructura = {
     abreParentesis: ["n", "("],
     cierraParentesis: ["o", ")"],
     punto: ["n"]
-};  
+};
+
+
+
+
+function reiniciar(){
+    entradaAnterior.letra = "";
+    entradaAnterior.catergoria = "";
+    entradaActual.letra = "";
+    entradaActual.categoria = "";
+    entrada.textContent = "";
+    resultado = 0;
+    contadorParentesis = 0;
+    estado = "correcto";
+    continuar = true;
+}
+
+
 
 
 // Agrega un evento de clic a cada botón
 botones.forEach(boton => {
-  boton.addEventListener("click", () => {
+    boton.addEventListener("click", () => {
 
-    entradaAnterior.letra = entradaActual.letra;
-    entradaAnterior.categoria = entradaActual.categoria;
+        entradaAnterior.letra = entradaActual.letra;
+        entradaAnterior.categoria = entradaActual.categoria;
+        const contenido = boton.querySelector("span").textContent;
+        entradaActual.letra = contenido;
+        entradaActual.categoria = identificarCategoria(entradaActual.letra);
 
-    const contenido = boton.querySelector("span").textContent;
 
-    entradaActual.letra = contenido;
-    entradaActual.categoria = identificarCategoria(entradaActual.letra);
+        if (entradaActual.categoria === "especial") {
+
+            switch (entradaActual.letra) {
+                case "C":
+                    reiniciar();
+                    break;
+                case "<":
+                    aux = entrada.textContent;
+                    aux = aux.slice(0, -1);
+                    entrada.textContent = aux;
+                    break;
+                case "=":
+                    //resultado = eval(entrada.textContent);
+                    //console.log("Resultado:", resutado);
+                    if(contadorParentesis === 0){
+
+                    }else{
+                        estado = "incorrecto";
+                        continuar = "Faltan parentesis";
+                        console.log(continuar);
+                    }
+                    break;
+                default:
+                    let prueba = "Especial no identificado";
+            }
+        }else{
+            entrada.textContent = entrada.textContent + contenido;
+            continuar = verificarReglas();
+            if (continuar === true) {
     
+            } else {
+                console.log(continuar);
+            }
+        }
 
-    entrada.textContent = entrada.textContent + contenido;
-
-    //console.log("Letra:", entradaActual.letra);
-    //console.log("Categoría:", entradaActual.categoria);
-
-    let continuar = verificarReglas();
-    if(continuar=== true){
-
-    }else{
-        console.log(continuar);
-    }
-  });
+        console.log("Letra:", entradaActual.letra);
+        console.log("Categoría:", entradaActual.categoria);
+    });
 });
 
-function identificarCategoria(caracter){
-    /*if(numeros.some(numero => numero === caracter) === true){
-        return "n";
-    }else if(caracter.some(operadores) === true){
-        return "o";
-    }else if(caracter.some(punto) === true){
-        return ".";
-    }else if(caracter.some(abreParentesis) === true){
-        return "(";
-    }else if(caracter.some(cierraParentesis) === true){
-        return ")";
-    }else if(caracter.some(especiales) === true){
-        return "especial";
-    }*/
 
+function identificarCategoria(caracter) {
 
     if (numeros.some(numero => numero === caracter)) {
         return "n";
@@ -74,44 +104,18 @@ function identificarCategoria(caracter){
     } else if (punto.some(p => p === caracter)) {
         return ".";
     } else if (abreParentesis.some(parentesis => parentesis === caracter)) {
+        contadorParentesis = contadorParentesis + 1;
         return "(";
     } else if (cierraParentesis.some(parentesis => parentesis === caracter)) {
+        contadorParentesis = contadorParentesis - 1;
         return ")";
     } else if (especiales.some(especial => especial === caracter)) {
         return "especial";
     }
-    
 }
 
-function verificarReglas(){
-    /*if(entradaAnterior.categoria === "n"){
-        if(!estructura.numero.includes(entradaActual.categoria)){
-            estado = "incorrecto";
-            return "Se esperaba: Numero u Operador";
-        }
-    }else if(entradaAnterior.categoria === "o"){
-        if(estructura.operador.some(numero => numero !== entradaActual.categoria)){
-            estado = "incorrecto";
-            return "Se esperaba: Numero o (";
-        }
-    }else if(entradaAnterior.categoria === "("){
-        if(estructura.abreParentesis.some(numero => numero !== entradaActual.categoria)){
-            estado = "incorrecto";
-            return "Se esperaba: Numero o (";
-        }
-    }else if(entradaAnterior.categoria === ")"){
-        if(estructura.cierraParentesis.some(numero => numero !== entradaActual.categoria)){
-            estado = "incorrecto";
-            return "Se esperaba: Operador o )";
-        }
-    }else if(entradaAnterior.categoria === "."){
-        if(estructura.punto.some(numero => numero !== entradaActual.categoria)){
-            estado = "incorrecto";
-            return "Se esperaba: Numero";
-        }
-    }*/
 
-
+function verificarReglas() {
 
     if (entradaAnterior.categoria === "n") {
         if (!estructura.numero.includes(entradaActual.categoria)) {
@@ -134,6 +138,6 @@ function verificarReglas(){
             return "Se esperaba: Numero";
         }
     }
-    
+
     return true;
 }
